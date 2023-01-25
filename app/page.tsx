0b529f8b8b5d9c12bbf4fc775be20d7588aba9bb
Mario Victor Medrano Maldonado <mariomedranomaldonado@gmail.com>
2023-01-25 23:01:58 +0000

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { education, hobbies, languages, workingExperience } from './data';
+import React from 'react';
+
 import { DescriptionSection } from './sections/DescriptionSection';
 import { EducationSection } from './sections/EducationSection';
 import { ExperienceSection } from './sections/ExperienceSection';
@@ -8,21 +9,21 @@ import { HobbySection } from './sections/HobbySection';
 import { LanguageSection } from './sections/LanguageSection';
 import { SkillSection } from './sections/SkillSection';
 
-const Page = () => {
-  return (
-    <>
-      <HomeHeader className="card" />
-      <main>
-        <DescriptionSection className="card" />
-        <ExperienceSection className="card mt-1" workingExperience={workingExperience} />
-        <SkillSection className="card mt-1" />
-        <EducationSection className="card mt-1" educationList={education} />
-        <LanguageSection className="card mt-1" languageList={languages} />
-        <HobbySection className="card mt-1" hobbies={hobbies} />
-      </main>
-      <HomeFooter className="card mt-1" />
-    </>
-  );
-};
+import { education, hobbies, languages, workingExperience } from './data';
+
+const Page = () => (
+  <>
+    <HomeHeader className="card" />
+    <main>
+      <DescriptionSection className="card" />
+      <ExperienceSection className="card mt-1" workingExperience={workingExperience} />
+      <SkillSection className="card mt-1" />
+      <EducationSection className="card mt-1" educationList={education} />
+      <LanguageSection className="card mt-1" languageList={languages} />
+      <HobbySection className="card mt-1" hobbies={hobbies} />
+    </main>
+    <HomeFooter className="card mt-1" />
+  </>
+);
 
 export default Page;
